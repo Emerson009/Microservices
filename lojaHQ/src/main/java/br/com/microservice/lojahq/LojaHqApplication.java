795d5f7e6537a6a96b826434a399ac456db06dab
Emerson009 <henriqueemerson023@gmail.com>
2021-11-16 07:06:01 -0300

    estudos-estrutura-inicial-microservices

---
+++ lojaHQ/src/main/java/br/com/microservice/lojahq/LojaHqApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.lojahq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaHqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaHqApplication.class, args);
+	}
+
+}
